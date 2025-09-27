d2e266dbd1a553e75e2887f6204812c8547a00b0
mpark1994 <mpark1994@outlook.com>
2025-09-27 18:02:48 -0400

    Completed UI, started logic

--- app/src/main/java/com/example/seg2105_group8_lab1/MainActivity.java
+++ app/src/main/java/com/example/seg2105_group8_lab1/MainActivity.java
@@ -1,24 +1,52 @@
 package com.example.seg2105_group8_lab1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9,
+            btnAdd, btnSub, btnMul, btnDiv, btnClr, btnEql, btnDec;
+
+    TextView display;
+
+    double val1, val2;
+
+    boolean add, sub, mul, div;
+
+    enum Operator {none, add, minus, multiply, divide};
+    Operator optr = Operator.none;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        display = findViewById(R.id.textViewValue);
+
+        btn0 = findViewById(R.id.button0);
+        btn1 = findViewById(R.id.button1);
+        btn2 = findViewById(R.id.button2);
+        btn3 = findViewById(R.id.button3);
+        btn4 = findViewById(R.id.button4);
+        btn5 = findViewById(R.id.button5);
+        btn6 = findViewById(R.id.button6);
+        btn7 = findViewById(R.id.button7);
+        btn8 = findViewById(R.id.button8);
+        btn9 = findViewById(R.id.button9);
+        btnAdd = findViewById(R.id.buttonAdd);
+        btnSub = findViewById(R.id.buttonSub);
+        btnMul = findViewById(R.id.buttonMult);
+        btnDiv = findViewById(R.id.buttonDiv);
+        btnDec = findViewById(R.id.buttonDot);
+        btnClr = findViewById(R.id.buttonClr);
+        btnEql = findViewById(R.id.buttonEquals);
+
     }
 }
